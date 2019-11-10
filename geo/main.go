0b3a83ff3bc476e7899e1b99fd5074a3dae279ec
chao <971064906@qq.com>
2019-11-11 00:07:58 +0800

    Add geo

---
+++ geo/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+var logger *log.Logger
+
+func init() {
+	f, err := os.OpenFile("./log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	logger = log.New(f, "geo", 0)
+}
+
+func main() {
+	http.HandleFunc("/", static)
+	http.HandleFunc("/post", post)
+	log.Fatalln(http.ListenAndServe("localhost:8002", nil))
+}
+
+func static(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("./index.html")
+	if err != nil {
+		http.Error(w, "Open file error: %s", http.StatusNoContent)
+	}
+	defer file.Close()
+	io.Copy(w, file)
+}
+
+func post(w http.ResponseWriter, r *http.Request) {
+	file, err := os.OpenFile("./data.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	if err != nil {
+		log.Fatalln(err)
+		http.Error(w, "Write file error", http.StatusNoContent)
+	}
+	defer r.Body.Close()
+	defer file.Close()
+	io.Copy(file, r.Body)
+	logger.Println(r.URL.Path)
+}
